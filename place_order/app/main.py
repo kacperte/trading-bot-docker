17bf420bfb72b9c00223b97d14377e1f6b61eeb1
Kacper <k.trzepiecinski@gmail.com>
2022-11-07 23:07:06 +0100

    add .env

--- place_order/app/main.py
+++ place_order/app/main.py
@@ -3,15 +3,14 @@ from okex_bot import OkexBot
 import time
 from db import SessionLocal, add_to_transaction_db, Base, engine
 
-# OKEX API credentials
-APIKEY = "ba435770-3228-4277-a71d-52ad5514514d"
-APISECRET = "DD9AB7614D44DF8DEAA7DEC0C216855C"
-PASS = "mD8SG#28vr2sAyB"
-BASEURL = "https://www.okex.com"
-
 # init Session
 session = SessionLocal()
 Base.metadata.create_all(engine)
+
+# init Redis
+redis = redis.StrictRedis(
+    host="redis", port=6379, charset="utf-8", decode_responses=True
+)
 
 
 class PlaceOrder(OkexBot):
@@ -28,7 +27,7 @@ class PlaceOrder(OkexBot):
         # using a for loop to perform this operation 10 times if the exchange will block operations
         for _ in range(9):
             # purchase of new token
-            order = self.place_market_order(
+            self.place_market_order(
                 pair=self.coin_id, side="buy", amount=self.usdt_balance
             )
             # if the purchase was successful, the loop will break
@@ -37,8 +36,8 @@ class PlaceOrder(OkexBot):
                 time.sleep(1)
                 # set order information
                 order_information = self.get_info_about_last_orders()[0]
-                # publish to redis information about selling strategy(%profit/%losses/%profit volumen/%losses volumen)
-                parms = [2, 0.8, 0.5, 1]
+                # publish to redis information about selling strategy
+                parms = [2, 0.8, 0.5, 1]  # %profit/%losses/%profit volumen/%losses volumen
                 parms = " ".join([str(i) for i in parms])
                 self.client.publish("new_position", parms)
                 # post to db inforamtion
@@ -55,18 +54,17 @@ class PlaceOrder(OkexBot):
 if __name__ == "__main__":
     # use loop to listen new message
     while True:
-        print("START LISTEN - REDIS PUB/SUB")
-        redis = redis.Redis(host="redis", port=6379)
+        print("LISTEN FOR NEW MESSAGE")
         sub = redis.pubsub()
-        sub.subscribe("token_id")
+        sub.subscribe("token")
         for message in sub.listen():
             if message["type"] == "message":
                 coind_id = str(message.get("data")).split("'")[1].split(" ")
                 print(coind_id)
-                # PlaceOrder(
-                #     coin_id=coind_id[0],
-                #     APISECRET=APISECRET,
-                #     APIKEY=APIKEY,
-                #     PASS=PASS,
-                #     BASEURL=BASEURL,
-                # )
+                PlaceOrder(
+                    coin_id=coind_id[0],
+                    APISECRET=APISECRET,
+                    APIKEY=APIKEY,
+                    PASS=PASS,
+                    BASEURL=BASEURL,
+                )
